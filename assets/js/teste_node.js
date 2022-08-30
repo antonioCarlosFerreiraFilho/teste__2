f99e05f6b248d1aa47dd8ed88a0614c0ac71d03e
Antonio Carlos <antoniozyzzcarlos@gmail.com>
2022-08-30 12:07:09 +0100

    novos arquivos node

---
+++ assets/js/teste_node.js
@@ -0,0 +1,13 @@
+let olamundo="ola";
+let indadeTony=24;
+var resultado;
+
+if(olamundo==="tony"){
+
+    resultado=(indadeTony*5);
+}else {
+
+    resultado=(indadeTony+3);
+}
+
+console.log(resultado);
